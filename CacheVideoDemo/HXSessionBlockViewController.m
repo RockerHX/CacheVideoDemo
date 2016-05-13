c19001e1c82ee42dbf6fe0355a54c5cc8ead4bd0
RockerHX <RockerHX@gmail.com>
2016-05-13 10:50:57 +0800

    Update Code

--- CacheVideoDemo/HXSessionBlockViewController.m
+++ CacheVideoDemo/HXSessionBlockViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "HXSessionBlockViewController.h"
+#import "HXDownLoadCache.h"
 
 
 @interface HXSessionBlockViewController ()
@@ -24,19 +25,7 @@
 - (IBAction)downButtonPressed {
     NSURL *url = [NSURL URLWithString:@"http://miadata1.ufile.ucloud.cn/piano_test/173.mp4"];
     
-    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReturnCacheDataElseLoad timeoutInterval:10.0f];
-    NSURLSession *session = [NSURLSession sharedSession];
-    NSCachedURLResponse *cachedResponse = [[NSURLCache sharedURLCache] cachedResponseForRequest:request];
-    NSLog(cachedResponse.data ? @"Cached response found!" : @"No cached response found.");
-    
-    //创建一个下载任务
-    NSURLSessionDownloadTask *task = [session downloadTaskWithRequest:request completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        NSLog(@"%@", location);
-        if (!cachedResponse.data) {
-            NSData *data = [NSData dataWithContentsOfURL:location];
-            NSCachedURLResponse *cachedURLResponse = [[NSCachedURLResponse alloc] initWithResponse:response data:data];
-            [[NSURLCache sharedURLCache] storeCachedResponse:cachedURLResponse forRequest:request];
-        }
+    [[HXDownLoadCache cache] downLoadWithURL:url completionHandler:^(NSData * _Nullable data, NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         
         dispatch_async(dispatch_get_main_queue(), ^{
             UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"下载成功！"
@@ -47,7 +36,6 @@
             [alert show];
         });
     }];
-    [task resume];
 }
 
 @end
